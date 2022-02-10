141d6d8cba737062bfa3695cc5bb00b05c65a201
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2022-02-10 10:56:52 +0100

    Add files via upload

--- index-script.js
+++ index-script.js
@@ -3,7 +3,7 @@ import { displayMessage } from "./components/displayMessage.js";
 const heroUrl = "http://localhost:1337/home";
 // const featured = document.querySelector(".featured-products");
 
-(async function() {
+async function getHero() {
   const container = document.querySelector(".hero");
 
   try {
@@ -11,14 +11,12 @@ const heroUrl = "http://localhost:1337/home";
     const products = await response.json();
     console.log(products);
     
-    products.forEach(function (data) {
-      container.innerHTML += `<div class="hero">
-                                      <img src="${data.hero_banner}"</img>
-                                      </div>`;
-      });
+    container.innerHTML += `<img src="${products.hero_banner}"</img>`;
     
   } catch(error) {
     console.log(error);
     displayMessage("error", error, ".featured-products");
   }
-})();
+};
+
+getHero();
